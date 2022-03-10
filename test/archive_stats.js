65214635e95682a9981b3a6d6c605ef3bc92c460
Mark Howe <mvahowe@gmail.com>
2022-03-10 16:01:07 +0100

    Refactor with one archive per file and as commonjs

---
+++ test/archive_stats.js
@@ -0,0 +1,11 @@
+const {
+    nt_1book,
+    nt_uw_1book,
+    nt_ebible_1book,
+    nt_2book
+} = require('../index');
+
+console.log("nt_1book", nt_1book.length);
+console.log("nt_uw_1book", nt_uw_1book.length);
+console.log("nt_ebible_1book", nt_ebible_1book.length);
+console.log("nt_2book", nt_2book.length);
